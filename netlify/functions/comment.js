5c06c3623fbe86c59ac1bd733f9e211e9e27dcdf
Austin Lee <i@imnerd.org>
2024-02-06 18:22:06 +0800

    chore: add env config for waline

--- netlify/functions/comment.js
+++ netlify/functions/comment.js
@@ -5,6 +5,7 @@ const Waline = require('@waline/vercel');
 const serverless = require('serverless-http');
 
 const app = Waline({
+  env: 'netlify',
   async postSave(comment) {
     // do what ever you want after save comment
   },
