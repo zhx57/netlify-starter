6a8d1729387815b574195c0c33a72255b2009143
ongao <zhx57@hotmail.com>
2025-10-30 11:55:26 +0800

    Create comment2.js

---
+++ functions/comment2.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const Waline = require('@waline/vercel');
+const serverless = require('serverless-http');
+
+const app = Waline({
+  env: 'netlify',
+  async postSave(comment) {
+    // do what ever you want after save comment
+  },
+});
+
+module.exports.handler = serverless(http.createServer(app));
