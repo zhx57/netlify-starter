7fa77033362996dd3e2c729de7c1897c17368daf
ongao <zhx57@hotmail.com>
2025-11-01 15:57:19 +0800

    Update comment.js

--- netlify/functions/comment.js
+++ netlify/functions/comment.js
@@ -4,6 +4,7 @@ const serverless = require('serverless-http');
 
 const app = Waline({
   env: 'netlify',
+  forbiddenWords: ['习近平', '毛泽东', '中国共产党', '免费节点'],
   async postSave(comment) {
     // do what ever you want after save comment
   },
